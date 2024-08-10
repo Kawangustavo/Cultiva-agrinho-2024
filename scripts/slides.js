d4c95cfc2d59e2927a8a4a7a15f413386e2cad18
Kawangustavo <106815992+Kawangustavo@users.noreply.github.com>
2024-08-09 23:20:27 -0300

    Alterações finais

--- scripts/slides.js
+++ scripts/slides.js
@@ -4,12 +4,14 @@ function carrosel() {
   var slides = document.getElementsByClassName("slide");
   for (var i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
+    x[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
   slides[slideIndex - 1].classList.add("active");
+  x[slideIndex-1].style.display = "block";
   setTimeout(carrosel, 2000);
 }
 
